f5c772fd0c6f2fd0206bc4b2dbffd21a160f861c
jkastle <56321810+jkastle@users.noreply.github.com>
2020-11-30 13:31:18 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+let menuToggler = document.querySelector(".nav__button");
+let navLinks = document.querySelectorAll(".nav-link");
+let body = document.querySelector("body");
+
+menuToggler.addEventListener("click", () => {
+    body.classList.toggle("open");
+});
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        body.classList.toggle("open");
+    });
+});
+
+$(document).ready(function () {
+    $(".carousel").slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        dots: true
+    });
+});
